ccb80e81521939b667f478891b71cd67ed7b7b59
Cole Hafner (Personal MacBook) <colehafner@gmail.com>
2016-05-06 07:18:34 -0700

    added font icons and tag input directive.

--- js/controllers.js
+++ js/controllers.js
@@ -4,8 +4,12 @@ angular.module('cjh')
 
 }])
 
-.controller('ProjectsCtrl', ['$scope', function($scope) {
+.controller('ProjectsCtrl', ['$scope', '$q', function($scope, $q) {
+		
 	var ctrl = this;
+	ctrl.selectedTags = [];
+	ctrl.tags = [];
+	
 	ctrl.projects = [{
 			name: 'Manifest Insights',
 			img: 'images/manifestinsights.com.png',
@@ -13,7 +17,8 @@ angular.module('cjh')
 			tags: ['angular', 'php', 'mysql', 'highcharts', 'html5', 'css', 'grunt', 'bower', 'composer'],
 			desc: 'Dynamic graphing tool able to connect to any database and provide a simple, intuitive UI for creating data dashboards. Integrates with Google Analytics and Salesforce.',
 			coolPoints: 1000,
-			role: 'Early Team Member/Current Project Lead'
+			role: 'Early Team Member/Current Project Lead',
+			visible: true
 			
 		}, {
 			name: '24 Hour Estimates',
@@ -22,7 +27,8 @@ angular.module('cjh')
 			tags: ['angular', 'php', 'mysql', 'highcharts', 'html5', 'css', 'grunt', 'bower', 'composer', 'vagrant', 'TCPDF', 'sass'],
 			desc: 'Utility to expedite the data entry of home inspection reports. Supports multiple user types and exports to PDF.',
 			coolPoints: 300,
-			role: 'Sole Contributor'
+			role: 'Sole Contributor',
+			visible: true
 		}, {
 			name: 'Win With Success',
 			img: 'images/winwithsuccess.com.png',
@@ -30,7 +36,8 @@ angular.module('cjh')
 			tags: ['angular', 'php', 'mysql', 'highcharts', 'html5', 'css', 'grunt', 'bower', 'composer', 'vagrant', 'sass'],
 			desc: 'Survey tool that helps companys determine their employee\'s perception of their brand.',
 			coolPoints: 200,
-			role: 'Project Lead'
+			role: 'Project Lead',
+			visible: true
 		}, {
 			name: 'Ecos Connect',
 			img: 'images/ecosconnect.com.png',
@@ -38,7 +45,8 @@ angular.module('cjh')
 			tags: ['angular', 'php', 'mysql', 'highcharts', 'html5', 'css', 'grunt', 'bower', 'composer', 'vagrant'],
 			desc: 'Designed for the backflow testing industry, this app streamlines the workflow between backflow testing companies and water suppliers.',
 			coolPoints: 800,
-			role: 'Project Lead'
+			role: 'Project Lead',
+			visible: true
 		}, {
 			name: 'Engoso Analytics',
 			img: 'images/ensogoanalytics.com.png',
@@ -46,7 +54,8 @@ angular.module('cjh')
 			tags: ['jquery', 'html', 'css', 'CORS'],
 			desc: 'Tool that analyzes and rates mutual funds based on their involvement/investment in fossil fuels. Deployable on any site with just a simple script tag. Fully customizable for each customer.',
 			coolPoints: 700,
-			role: 'Sole Contributor/Lead Developer'
+			role: 'Sole Contributor/Lead Developer',
+			visible: true
 		}, {
 			name: 'Medipro Direct',
 			img: 'images/mediprodirect.com.png',
@@ -54,7 +63,8 @@ angular.module('cjh')
 			tags: ['jquery','php', 'html', 'css'],
 			desc: 'Made for the life insurance industry, this app manages life insurance policies from start to finish.',
 			coolPoints: 600,
-			role: 'Team member turned Project Lead'
+			role: 'Team member turned Project Lead',
+			visible: true
 		}, {
 			name: 'Quality First',
 			img: 'images/qualityplacespolicies.com.png',
@@ -62,7 +72,8 @@ angular.module('cjh')
 			tags: ['jquery','php', 'html', 'css', 'angular', 'CORS', 'grunt'],
 			desc: 'Sister site for MediPro that collects and analyzes data gathered via custom quality metrics email campaign.',
 			coolPoints: 650,
-			role: 'Project Lead'
+			role: 'Project Lead',
+			visible: true
 		}, {
 			name: 'Grubbable App',
 			img: 'images/grubbable.com.png',
@@ -70,10 +81,43 @@ angular.module('cjh')
 			tags: ['angular','grunt', 'android', 'iOS', 'ionic', 'CORS', 'mobile', 'cross-platform', 'html5', 'css', 'sass'],
 			desc: 'Restaraunt rating app for iOS and Android. Built with using Ionic mobile development framework.',
 			coolPoints: 770,
-			role: 'Team Member'
+			role: 'Team Member',
+			visible: true
 		}
-		
 	];
+   
+	ctrl.loadTags = function(query) {
+		var deferred = $q.defer();
+		//ctrl.tags.filter(function(tag) {return (tag && tag.text.indexOf(query) > -1);});
+		deferred.resolve(ctrl.tags);
+		return deferred.promise;
+	};
+	
+	ctrl.filterProjects = function($tag) {
+		for (var i = 0, len = ctrl.projects.length; i < len; ++i) {
+			ctrl.projects[i].visible = (ctrl.projects[i].tags.indexOf($tag.text) > -1);
+		}
+	};
+	
+	//find unique list of tags
+	var uniqueTags = [];
+	
+	for (var i = 0, len = ctrl.projects.length; i < len; ++i) {
+		var proj = ctrl.projects[i];
+		
+		for (var j = 0, len = proj.tags.length; j < len; ++j) {
+			if (uniqueTags.indexOf(proj.tags[j]) === -1) {
+				ctrl.tags.push({text: proj.tags[j]});
+				uniqueTags.push(proj.tags[j]);
+			}
+		}
+	}
+	
+	$scope.$watch('ctrl.search', function(newVal, oldVal) {
+		if (!newVal || newVal.length <= 1) {
+			return;
+		}
+	});
 }])
 
 .controller('BlogCtrl', ['$scope',  function($scope) {
